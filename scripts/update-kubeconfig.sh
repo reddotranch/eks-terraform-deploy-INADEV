f4620ad0a903707314ac8ec0d32402a90908787b
TDW <mbwork1@yahoo.com>
2025-01-21 01:56:32 -0500

    update kubeconfig

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-response="$(aws eks list-clusters --region us-east-2 --output text | grep -i tdw-cluster 2>&1)" 
+response="$(aws eks list-clusters --region us-east-2 --output text | grep -i inadev-cluster 2>&1)" 
 if [[ $? -eq 0 ]]; then
-    echo "Success: TDW-cluster exist"
-    aws eks --region us-east-2 update-kubeconfig --name tdw-cluster && export KUBE_CONFIG_PATH=~/.kube/config
+    echo "Success: inadev-cluster exist"
+    aws eks --region us-east-2 update-kubeconfig --name inadev-cluster && export KUBE_CONFIG_PATH=~/.kube/config
 
 else
-    echo "Error: TDW-cluster does not exist"
+    echo "Error: inadev-cluster does not exist"
 fi
