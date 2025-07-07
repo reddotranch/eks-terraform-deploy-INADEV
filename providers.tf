2081aa9cff8aded59e5fe211dfc74e4e231da9d9
DevUser <betechincorporated@gmail.com>
2025-07-07 22:47:42 +0000

    update root providers

--- providers.tf
+++ providers.tf
@@ -15,7 +15,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
