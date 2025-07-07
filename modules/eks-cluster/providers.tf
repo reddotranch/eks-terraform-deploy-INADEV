9ffc3a1cfa3288a1f8c77e631cf1b30eb0125d86
DevUser <betechincorporated@gmail.com>
2025-07-07 22:01:18 +0000

    update alb provider helm added

--- modules/eks-cluster/providers.tf
+++ modules/eks-cluster/providers.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+  }
+}
+
